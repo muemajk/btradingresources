d11c85690c5e8bb83626ee053bf94e940e60fa70
unknown <you@example.com>
2019-06-13 10:09:55 +0300

    changes

--- btresources/settings/production.py
+++ btresources/settings/production.py
@@ -2,7 +2,7 @@ from btresources.settings.common import *
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 #SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '!we0ag^yw*a)3(!vqq3iab%s5enj1#plikv2qo8cp(tzi891ve')
 # SECURITY WARNING: don't run with debug turned on in production!
