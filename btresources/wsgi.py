43531f960810185f94b3b5fd885d06e793aacc6c
unknown <you@example.com>
2019-06-13 10:15:17 +0300

    changes

--- btresources/wsgi.py
+++ btresources/wsgi.py
@@ -12,7 +12,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from dotenv import load_dotenv
 
-project_folder = os.path.expanduser('btresources')  # adjust as appropriate
+project_folder = os.path.expanduser('btresources/btresources')  # adjust as appropriate
 load_dotenv(os.path.join(project_folder, '.env'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'btresources.settings.production')
