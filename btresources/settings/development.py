3e121aea16c56b1ad8df14f87dfc61e67b944bc2
muemajk <muema31jk@gmail.com>
2019-07-13 01:00:21 +0300

    Update development.py

--- btresources/settings/development.py
+++ btresources/settings/development.py
@@ -1,8 +1,7 @@
 from btresources.settings.common import *
 
 
-# SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = '!we0ag^yw*a)3(!vqq3iab%s5enj1#plikv2qo8cp(tzi891ve'
+
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '!we0ag^yw*a)3(!vqq3iab%s5enj1#plikv2qo8cp(tzi891ve')
 # SECURITY WARNING: don't run with debug turned on in production!
